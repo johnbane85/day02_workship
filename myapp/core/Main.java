df71e8ca3d6972298a62f1ffe8cf366635428196
johnbane85 <junyu.siew@gmail.com>
2022-06-01 20:13:50 +0800

    another update now

--- myapp/core/Main.java
+++ myapp/core/Main.java
@@ -3,7 +3,9 @@ package myapp.core;
 public class Main {
   public static void main (String[] args) {
     BankAccount firstAccount = new BankAccount("Calvin");
-    BankAccount secondAccount = new BankAccount("James", 1000.50f);
-  
+    
+    BankAccount secondAccount = new BankAccount("James", 50f);
+
+    BankAccount thirdAccount = new BankAccount("John", 790f);
   }
 }
